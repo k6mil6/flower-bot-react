654afb0402051449aa33bbd09e6b9ce2c500e590
k6mil6 <kamil_6@vk.com>
2023-07-08 22:02:23 +0300

    innit

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './ProductList.css';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                ProductList
+            </div>
+        );
+    }
+}
+
+export default ProductList;
