87ac918f3162e18b77302f756baf8932c57478b9
k6mil6 <kamil_6@vk.com>
2023-07-09 14:08:06 +0300

    innit

---
+++ src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import productList from "../ProductList/ProductList";
+import Button from "../Button/Button";
+import './ProductItem.css';
+
+const ProductItem = ({product, className, onAdd}) => {
+
+    const onAddHandler = () => {
+        onAdd(product);
+    }
+
+    return (
+        <div className={'product' + className}>
+            <div className={'img'}/>
+            <div className={'title'}>{product.title}</div>
+            <div className={'description'}>{product.description}</>
+            <div className={'price'}>
+                <span>Стоимость: <b>{prduct.price}</b></span>
+            </div>
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
+            </Button>
+        </div>
+    );
+};
+
+export default ProductItem;
